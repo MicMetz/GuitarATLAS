bc142765d5c1a3a5fe0992cbfdce0b9d7c64b44f
MicMetz <mime9599@colorado.edu>
2021-03-20 19:24:47 -0600

    Add files via upload

---
+++ src/FixedSizeQueue.java
@@ -0,0 +1,243 @@
+/*
+ #############################################################################
+ ###                                                                       ###
+ ### Title:         Guitar Hero                                            ###
+ ###                                                                       ###
+ ### Files:         FixedSizeQueue.java            	                       ###
+ ### Author(s):     Michael Metz (mime9599@colorado.edu)                   ###
+ ### Semester:      Spring 2021                                            ###
+ ### Written:       March 16, 2021                                         ###
+ ### Description:   Simulates plucking a guitar string using the           ###
+ ###				 Karplus-Strong algorithm                              ###
+ ### License:                                                              ###
+ ### Credits:                                                              ###
+ #############################################################################
+ */
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * A generic Queue with a fixed capacity implemented via a linked-list
+ * following a FIFO policy. It would actually be more efficient to implement
+ * this using an array (i.e., as a ring buffer), but we will implement it as
+ * a linked-list instead since the implementation is simpler. Re-implementing this
+ * as a ring buffer would be a great exercise if you want to push yourself - if so,
+ * see the course slides as a reference.
+ *
+ * @param <T> The type of object that the FixedSizeQueue will hold
+ * @author Michael M
+ */
+public class FixedSizeQueue<T> implements Iterable<T> {
+
+	/**
+	 * Internal node class for the linked-list data structure
+	 *
+	 * @author Michael M
+	 */
+	private class Node {
+		T    value; // The data the node holds
+		Node next; // A pointer to the next node in the list (or null if this is the last node)
+
+
+
+		/**
+		 * Create a new Node
+		 *
+		 * @param value The value this node should store
+		 */
+		public Node(T value) {
+			this.value = value;
+			this.next = null;
+		}
+	}
+
+	// Instance data
+	private Node head, tail; // Keep track of the first and last node in the list
+	private       int size;
+	private final int capacity; // Keep track of how many nodes are in the list and our total capacity
+
+
+
+	/**
+	 * Create a new FixedSizeQueue with a specified capacity
+	 * <p>
+	 * Pseudocode:
+	 * All you have to do here is set the capacity instance variable
+	 *
+	 * @param capacity How many items this queue will be able to hold
+	 */
+	public FixedSizeQueue(int capacity) {
+		this.capacity = capacity;
+		size = 0;
+	}
+
+
+
+	/**
+	 * Get the number of nodes in the queue
+	 *
+	 * @return the number of nodes in the queue
+	 */
+	public int size() {
+		return this.size;
+	}
+
+
+
+	/**
+	 * Check if the queue is empty
+	 *
+	 * @return true if the queue is empty (has no nodes), false otherwise
+	 */
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+
+
+	/* Check if the queue is empty
+	 *
+	 * @return true if the queue is full (size == capacity), false otherwise
+	 */
+	public boolean isFull() {
+		return size == capacity-1;
+	}
+
+
+
+	/**
+	 * Add a new node based on a given value to the end of the queue.
+	 * Psuedocode:
+	 * 1. If the queue is already full (i.e., if the size equals the capacity):
+	 * A. Throw a new IllegalStateException with a helpful message (e.g., stating
+	 * that the queue is already full)
+	 * [if you are struggling, see the syntax in dequeue below for an example
+	 * of how to throw exceptions and provide a message]
+	 * 2. If the list is currently empty:
+	 * A. Create a new Node based on the passed in value
+	 * B. Set your head and tail pointers to this new node
+	 * 3. Else:
+	 * A. Save the old tail pointer to a temporary variable
+	 * B. Create a new Node based on the passed in value and set the tail
+	 * to be this new node
+	 * C. Set the .next value of the temporary variable (which is equal to the
+	 * node that used to be the tail node) to point to the new tail node
+	 * 4. Increment the size counter
+	 *
+	 * @param value The given value, which will be used to create a new node that is added
+	 *              to the end of the queue.
+	 * @throws IllegalStateException if the queue is already full (i.e., if the size equals
+	 *                               the capacity)
+	 */
+	public void enqueue(T value) {
+		if (isFull()) { System.out.println("Error - " + "can't enqueue. The queue is full."); }
+		else {
+			if (isEmpty()) {
+				head = new Node(value);
+				tail = head;
+			}
+			else {
+				Node temp = new Node(value);
+				tail.next = temp;
+				tail = temp;
+			}
+			this.size++;
+		}
+	}
+
+
+
+	/**
+	 * Removes the first node in the list and returns its value.
+	 * Pseudocode:
+	 * 1. Create a temporary variable to store the head node
+	 * 2. Set the head node to be the next node in the queue
+	 * 3. Decrement the size counter
+	 * 4. Return the value in the temporary variable that stores the
+	 * old head node
+	 *
+	 * @return The value of the first node in the list
+	 * @throws NoSuchElementException if the list is empty
+	 */
+	public T dequeue() {
+		if (!isEmpty()) {
+			if (!isFull()) {
+				T val = head.value;
+				if (head == tail) {
+					tail = null;
+					head = null;
+				} else {
+					head = head.next;
+				}
+				size--;
+				return val;
+			}
+			else { throw new NoSuchElementException("Error - " + "can't dequeue. The queue is empty."); }
+		}
+		else { throw new NoSuchElementException("Error - " + "can't dequeue. The queue is empty."); }
+	}
+
+
+
+	/**
+	 * Return the value of the first node in the queue without removing that node from the
+	 * queue.
+	 * <p>
+	 * Pseudocode:
+	 * 1. Check if the queue is empty and, if so, throw a NoSuchElementException just as
+	 * in dequeue
+	 * <p>
+	 * 2. Return the value stored in the head node
+	 *
+	 * @return The value of the first node in the queue
+	 * @throws NoSuchElementException if the queue is empty
+	 */
+	public T peek() {
+		//		if (size == 0) throw new NoSuchElementException("Error - " + "can't peek as the queue is empty!");
+		if (isEmpty()) { return null; }
+		return head.value;
+	}
+
+
+
+	/**
+	 * TODO
+	 */
+	@Override
+	public Iterator<T> iterator() {
+		return new ListIterator();
+	}
+
+
+
+	/**
+	 * TODO
+	 * expand
+	 */
+	private class ListIterator implements Iterator<T> {
+
+		// The current node we are iterating over (start at the front of the queue)
+		private Node current = head;
+
+
+
+		// The Iterator methods
+		public boolean hasNext() {
+			return current != null;
+		}
+
+
+
+		public void remove() {  /* not supported */ }
+
+
+
+		public T next() {
+			T value = current.value;
+			current = current.next;
+			return value;
+		}
+
+	}
+}
---
+++ src/GuitarString.java
@@ -0,0 +1,144 @@
+/*
+ #############################################################################
+ ###                                                                       ###
+ ### Title:         Guitar Hero                                            ###
+ ###                                                                       ###
+ ### Files:         GuitarString.java            	                       ###
+ ### Author(s):     Michael Metz (mime9599@colorado.edu)                   ###
+ ### Semester:      Spring 2021                                            ###
+ ### Written:       March 16, 2021                                         ###
+ ### Description:   A single guitar string simulated using the             ###
+ ###				 Karplus-Strong algorithm                              ###
+ ### License:                                                              ###
+ ### Credits:                                                              ###
+ #############################################################################
+ */
+
+
+import java.util.Iterator;
+
+/**
+ * This class represents a single guitar string or piano wire, which is simulated using
+ * the Karplus-Strong algorithm. When a guitar string is plucked or a piano wire struck,
+ * it vibrates and creates sound. The vibration can be measured by sampling the
+ * displacement of the wire at equally spaced points. These displacement measurements
+ * can be stored digitally, say in a list or queue structure, then used to recreate
+ * the sound wave over a speaker.
+ * <p>
+ * More specifically, the length of a string or wire determines its fundamental frequency
+ * of vibration. We model a guitar string by sampling its displacement
+ * (a real number between -1/2 and +1/2) at N equally spaced points (in time),
+ * where N equals the sampling rate (44,100) divided by the fundamental frequency
+ * (rounding the quotient up to the nearest integer)
+ *
+ * @author Michael M
+ */
+public class GuitarString {
+
+	// Karplus-Strong energy decay factor
+	public static final double ENGERGY_DECAY_FACTOR = 0.994;
+
+	// Queue to hold the displacement values at equally-spaced points along the guitar string
+	private FixedSizeQueue<Double> queue;
+
+	// Number of time steps that have been simulated
+	private int numTics;
+
+
+
+	/**
+	 * Create a new guitar string for a certain frequency. For example, setting
+	 * frequency to 440 would give you a string tuned to Concert A.
+	 * <p>
+	 * Psuedocode:
+	 * 1. Calculate N: the length of the queue you will need for the specified frequency
+	 * where N = the sampling rate 44,100 divided by frequency, rounded up to the
+	 * nearest integer
+	 * <p>
+	 * 2. Initialize your queue using this calculated capacity N and initialize
+	 * the queue to represent a guitar string at rest by enqueueing N zeros.
+	 *
+	 * @param frequency The frequency for the guitar string
+	 */
+	public GuitarString(double frequency) {
+		int N = (int) Math.ceil(44100 / frequency);
+		queue = new FixedSizeQueue<>(N);
+
+		for (int i = 0; i < queue.size(); i++) {
+			queue.enqueue(0.0);
+		}
+	}
+
+
+
+	/**
+	 * Simulates plucking a guitar string.
+	 * <p>
+	 * Psuedocode:
+	 * Set the queue to white noise by replacing all N items in the queue with N random
+	 * values between -0.5 and +0.5
+	 * <p>
+	 * TODO
+	 * iterate over queue
+	 */
+	public void pluck() {
+		for (int i = 0; i < queue.size(); i++) {
+			queue.dequeue();
+			queue.enqueue(Math.random() * ((-0.5 - 0.5) + 1) + -0.5);
+		}
+	}
+
+
+
+	/**
+	 * Simulates one time step by applying the Karplus-Strong update.
+	 * <p>
+	 * Psuedocode:
+	 * 1. Remove the sample at the front of the queue, storing its value in a temporary variable
+	 * <p>
+	 * 2. Add to the end of the queue the average of the first two samples (i.e., the sample you
+	 * just removed and the sample that is now at the front of the queue)
+	 * multiplied by the energy decay factor.
+	 * <p>
+	 * 3. Update numTics
+	 */
+	public void tic() {
+		double temp;
+
+		if (queue.isEmpty()) {
+			System.out.println("Error - " + "can't dequeue. The queue is empty.");
+		} else {
+			temp = queue.dequeue();
+			if (queue.isFull()) { System.out.println("Error - " + "can't enqueue. The queue is full."); } else {
+				queue.enqueue(((temp + queue.peek()) / 2) * ENGERGY_DECAY_FACTOR);
+				numTics++;
+			}
+		}
+	}
+
+
+
+	/**
+	 * Get the sample value that is at the front of the queue (do not remove this value
+	 * from the queue).
+	 *
+	 * @return The sample value currently at the front of the queue
+	 */
+	public double sample() {
+		if (queue.isEmpty()) { return 0.0; }
+		return queue.peek();
+	}
+
+
+
+	/**
+	 * Gets the total number of times tic() has been called (i.e., the numer of time-steps
+	 * we have simulated so far).
+	 *
+	 * @return The number of times the tic() method has been called
+	 */
+	public int time() {
+		return numTics;
+	}
+
+}
